80946dfe119af54524c689a47bfddf93a4986151
Mahmudul Hasan <148671264+hasanfrombd@users.noreply.github.com>
2024-06-27 20:15:06 +0600

    firstCommit

---
+++ script/script.js
@@ -0,0 +1,27 @@
+
+// javaScript Events Practice!
+
+// event: onclick;
+function onClick() {
+    document.getElementById("titleTwo").innerHTML = 'Subscribed!';
+}
+
+// event: ondblclick;
+function onDblClick() {
+    document.getElementById("titleThree").innerHTML = 'Subscribed! Thanks!';
+}
+
+// event: oncontextmenu;
+function onContextMenu() {
+    document.getElementById("titleFour").innerHTML = 'Subscribed! with oncontexmenu (mouse right click)!';
+}
+
+// event: onmouseenter!;
+function onMouseEnter() {
+    document.getElementById("titleFive").innerHTML = 'Subscribed when you entered mouse!';
+}
+
+// event: onmouseout!;
+function onMouseOut() {
+    document.getElementById("titleSix").innerHTML = 'Subscribed when you took out your mouse!';
+}
